21771b92fa6d6f57a385dc7ff99d75330f153a8d
KenyaPeterson <kenyap013@gmail.com>
2024-11-21 20:05:22 -0500

    add formValidator

---
+++ components/FormValidator.js
@@ -0,0 +1,14 @@
+/* class formValidator {
+    constructor(settings, form) {
+        this.settings = settings;
+        this._form = form;
+        this.inputs = form.querySelectorAll('input');
+        this.submitButton = form.querySelector('button[type="submit"]');
+        this._init();
+
+    }
+
+    enableValidation() {
+        this._setEventListeners();
+    }
+} */
